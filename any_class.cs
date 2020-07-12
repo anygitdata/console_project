d2c8ada02ee7d45ece3b317c54d298bfe4cae809
Vladimir <rez-data@rambler.ru>
2020-07-12 21:06:10 +0300

    #1 Пробное создание первичной копии
    Добавление класса any_class

---
+++ any_class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_cons
+{
+    /// <summary>
+    /// класс для тестирования github
+    /// </summary>
+    public class any_class
+    {
+
+    }
+}
